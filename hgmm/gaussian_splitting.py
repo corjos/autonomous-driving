fixed calculation of the hgmm linearity metric

hgmm/gaussian_splitting.py
@@ -56,6 +56,10 @@ def get_rot_mat_from_vects (x, y):
     ''' Find an n-dimensional rotation matrix between two n-length vectors x and y '''
     assert max(y.shape) == max(x.shape)
     n = max(x.shape)
+
+    if np.allclose(x, y):
+        return np.eye(n)
+        
     x = np.reshape(x, (n,1)) #/ np.linalg.norm(x)
     y = np.reshape(y, (n,1)) #/ np.linalg.norm(y)
     
@@ -80,7 +84,7 @@ def get_rot_mat_from_vects (x, y):
         print 'x:', x.flatten(),'y:', y.flatten()
         print R
         print np.dot(R, R.T)
-        #raise Exception
+        raise Exception
     
     return R
 

hgmm/gmm.py
@@ -69,10 +69,18 @@ class GaussianMixture:
             ret += 'w: {}, mu: {}, P {}\n'.format(m.w, m.mu, m.P)
         return ret
         
-    def plot_ellipse(self):
+    def copy(self):
+        ''' Return a shallow copy of the mixture'''
+        return GaussianMixture(self.mixands.copy())
+        
+    def plot_ellipse(self, **kwargs):
         ''' plot first two dimensions of the mixture'''
+        ellipses = []
         for m in self.mixands:
-            cvre.plot_cov_ellipse(m.P[:2,:2], m.mu[:2], alpha=m.w)
+            p = cvre.plot_cov_ellipse(m.P[:2,:2], m.mu[:2], alpha=m.w, **kwargs)
+            ellipses.append(p)
+        return ellipses
+        
             
     def plot_heatmap(self, nstd=10, nsteps=200, cmap=cm.gray_r):
         ''' plot first two dimensions of the mixture'''
@@ -80,9 +88,9 @@ class GaussianMixture:
         for m in self.mixands:
             # find image extents
             xmin = min(xmin, m.mu.flatten()[0] - nstd*np.sqrt(np.diag(m.P)[0]))
-            xmax = max(xmin, m.mu.flatten()[0] + nstd*np.sqrt(np.diag(m.P)[0]))
+            xmax = max(xmax, m.mu.flatten()[0] + nstd*np.sqrt(np.diag(m.P)[0]))
             ymin = min(ymin, m.mu.flatten()[1] - nstd*np.sqrt(np.diag(m.P)[1]))
-            ymax = max(ymin, m.mu.flatten()[1] + nstd*np.sqrt(np.diag(m.P)[1]))
+            ymax = max(ymax, m.mu.flatten()[1] + nstd*np.sqrt(np.diag(m.P)[1]))
 
         Z = 0
         lim = (xmin, xmax, ymin, ymax)

hgmm/hgmm.py
@@ -130,8 +130,9 @@ def propagate(mixture, func, k, cov_v, e_res_max=0.5, max_mixands=30):
         Y = pts[n_x:,:]
         
         # Calcs to help find splitting axis
-        Xbar, Xbar_weights = get_sigma_points(m.mu, m.P) # n_x by 2*n_x + 1
+        Xbar, _ = get_sigma_points(m.mu, m.P) # n_x by 2*n_x + 1
         Xbar_augmented = np.concatenate( ( Xbar, np.ones((1,2*n_x+1)) )  )
+        assert Xbar_augmented.shape == (n_x+1, 2*n_x+1)
         # LQ factorisation
         Q, R = np.linalg.qr(Xbar_augmented.T, mode='complete') 
         Q = Q.T
@@ -139,23 +140,21 @@ def propagate(mixture, func, k, cov_v, e_res_max=0.5, max_mixands=30):
 
         # Propagate sigma points and get new mean, cov, and sigma points
         X_new = func(X, Y, k)
-        mu_x_new, cov_x_new = get_sigma_points_mean_cov(X_new, weights)
         
-        Xbar_new, Xbar_new_weights = get_sigma_points(mu_x_new, cov_x_new) # sigma points relating to state
+        Xbar_new = func(Xbar, np.zeros((n_v, 2*n_x+1)), k) # sigma points relating to state
         Xhat_all_new = np.dot(Xbar_new, Q.T)
         Xhat_res_new = Xhat_all_new[:,n_x+1:] # X_hat_all partition not explained by linear model
     
         # Assign stuff
-        m.mu = mu_x_new
-        m.P = cov_x_new            
+        m.mu, m.P = get_sigma_points_mean_cov(X_new, weights)
             
         # Calculate linearisation error residual e_res (mixand splitting criteria)
         zeromat = np.zeros((n_x,n_x+1))
-        e_res = np.linalg.norm( np.concatenate((zeromat, Xhat_res_new), axis=1) ) 
+        e_res = np.linalg.norm( np.concatenate((zeromat, Xhat_res_new), axis=1))
         
         print 'e_res =', e_res,
         if e_res > e_res_max:
-            print ' splitting....'
+            print ' splitting....',
             # Residual associated with each sigma point
             E_new = np.dot( np.concatenate( (zeromat, Xhat_res_new), axis=1 ),
                             Q )
@@ -176,6 +175,7 @@ def propagate(mixture, func, k, cov_v, e_res_max=0.5, max_mixands=30):
             # Remove current mixand and add split results
             mixture.mixands.remove(m)
             mixture.mixands.update(new_mixands)
+            print 'done'
         else:
             print ''
         
@@ -185,7 +185,7 @@ def propagate(mixture, func, k, cov_v, e_res_max=0.5, max_mixands=30):
         
 def f(x, v=np.array([[0]]), k=0):
     ''' Generic nonlinear discrete-time dynamics function for testing purposes'''
-    r = 10 - k/49
+    r = 10 #- k/49
     vel = np.pi*r/200 + v*np.ones_like(x[0,:])
     dt = 1
     
@@ -248,7 +248,7 @@ if __name__ == "__main__":
         print '\n timestep:', k
         print 'N =', mixture.N   
         
-        propagate(mixture, f, k, cov_v, e_res_max=7e-15, max_mixands=10)
+        propagate(mixture, f, k, cov_v, e_res_max=0.5, max_mixands=10)
 
         mixture.plot(nsteps=200)
         plt.pause(0.01)